67d0460a4957723a7863fb7b3989b9bc5699379f
ManduleeLaknara <mandulilkr2020@gmail.com>
2026-04-28 14:20:26 +0530

    fix: stripe webhook raw body before express.json

--- server/server.js
+++ server/server.js
@@ -2,42 +2,42 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/db.js";
- 
+
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
- 
+
 import userRouter from "./routes/userRoutes.js";
 import hotelRouter from "./routes/hotelRoutes.js";
 import roomRouter from "./routes/roomRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
 import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
- 
+
 connectDB();
- 
+
 const app = express();
- 
+
+
+app.post('/api/stripe', express.raw({type: "application/json"}), stripeWebhooks)
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
- 
+
 app.use(cors({
     origin: "*",
     credentials: false
 }));
 
-// API to listen to Stripe Webhooks
-app.post('/api/stripe', express.raw({type: "application/json"}), stripeWebhooks)
- 
 app.use(clerkMiddleware());
- 
+
 app.use("/api/clerk", clerkWebhooks);
- 
+
 app.get("/", (req, res) => res.send("API is working"));
- 
+
 app.use("/api/user", userRouter);
 app.use("/api/hotels", hotelRouter);
 app.use("/api/rooms", roomRouter);
 app.use("/api/bookings", bookingRouter);
- 
+
 const PORT = process.env.PORT || 3000;
- 
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
